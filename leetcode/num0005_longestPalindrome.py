78c5efc1a99c33dd2d81ca1cca9fb38d9583d2b3
freshield <zxdsw199182@gmail.com>
2020-04-17 23:37:15 +0800

    done the num0005 dp solution

--- leetcode/num0005_longestPalindrome.py
+++ leetcode/num0005_longestPalindrome.py
@@ -14,6 +14,7 @@
 @                                    Freshield @
 @==============================================@
 """
+import numpy as np
 
 
 def is_valid_palindrome(test_str, begin, end):
@@ -64,9 +65,80 @@ def longestPalindrome_force(test_str):
     return rst_str
 
 
-if __name__ == '__main__':
-    test_str0 = 'babadaaaaaaaaaaaaaaaaaaaa'
-    test_str1 = 'cbbdaaaaaaaaaaaaaaaaaaaaa'
+def longestPalindrome_dp(test_str):
+    """
+    使用动态规划的方法来进行回文子串选择
+    解法：
+    用dp方法主要在于最内部的遍历时如何对字串是回文的判定进行优化
+    1. dp表示什么：dp[i, j]表示test_str[i:j]是否有回文字串
+    2. dp关联条件：
+        i. 如果si != sj则回文失败
+        ii. 如果j - i < 3则为回文，因为一个字符或者两个字符肯定是回文
+        iii. 如果si == sj, 则dp[i, j] = dp[i+1, j-1]
+    3. dp初始条件：dp[i, i]都为True，因为标识为一个字符
+    4. dp如何遍历：设置的dp矩阵为(i,j)矩阵，大小为(N, N)N为字符串长度
+        j是依赖于上方的数据，i是依赖于右方的数据，
+        所以我们的遍历j需要从小到大，i需要从大到小
+    整体流程：
+    1. 判别test_str长度是否小于3
+    2. 构建dp矩阵以及其他中间变量，初始化dp
+    3. 遍历i也就是left
+    4. 遍历j也就是right
+    5. 根据条件来进行判断
+    6. 返回最长回文长度
+    """
+    # 1. 判别test_str长度是否小于3
+    str_size = len(test_str)
+    if str_size < 3:
+        return str_size
 
-    print(longestPalindrome_force(test_str0))
-    print(longestPalindrome_force(test_str1))
+    # 2. 构建dp矩阵以及其他中间变量
+    dp = np.ones((str_size, str_size), dtype=np.int8) * -1
+    for i in range(str_size):
+        dp[i, i] = True
+    max_len = 1
+    max_left = -1
+    max_right = -1
+
+    # 3. 遍历i也就是left
+    for left in range(str_size-2, -1, -1):
+        # 4. 遍历j也就是right
+        for right in range(left+1, str_size):
+            # print('left', left)
+            # print('right', right)
+            # 5. 根据条件来进行判断
+            if right - left + 1 < 3:
+                # print('s0')
+                dp[left, right] = 1
+            elif test_str[left] != test_str[right]:
+                # print('s1')
+                dp[left, right] = 0
+            else:
+                # print('s2')
+                dp[left, right] = dp[left + 1, right - 1]
+
+            if dp[left, right] == 1:
+                if (right - left + 1) > max_len:
+                    max_len = right - left + 1
+                    max_left = left
+                    max_right = right
+
+            # print()
+
+
+    # 6. 返回最长回文长度
+    # print(max_left)
+    # print(max_right)
+    # print(dp)
+    return test_str[max_left: max_right+1]
+
+
+
+if __name__ == '__main__':
+    test_str0 = 'babad'
+    test_str1 = 'cbbda'
+
+    # print(longestPalindrome_force(test_str0))
+    # print(longestPalindrome_force(test_str1))
+    print(longestPalindrome_dp(test_str0))
+    print(longestPalindrome_dp(test_str1))
