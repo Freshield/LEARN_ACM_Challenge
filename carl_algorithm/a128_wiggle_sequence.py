9be7b79d17c97f07c68daab93a4f06726dec8d59
freshield <yangyufresh@163.com>
2022-08-02 09:59:07 +0800

    wiggle sequence

---
+++ carl_algorithm/a128_wiggle_sequence.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+"""
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a128_wiggle_sequence.py
+@Time: 2022-10-19 20:37
+@Last_update: 2022-10-19 20:37
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+
+
+def wiggle_sequence(nums):
+    """
+    摆动序列
+    """
+    pass
+
+
+if __name__ == '__main__':
+    nums = [1, 7, 4, 9, 2, 5]
+    print(wiggle_sequence(nums))
